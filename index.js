4b9b79d96c38a5500ee5457afc6f227658aa10bd
DayanSesami <dayan.nilisani@sesami.co>
2023-09-20 18:49:49 +0330

    index js updated

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,13 +35,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const redis_1 = __importDefault(require("redis"));
-const mysql_1 = __importDefault(require("mysql"));
+const redis = __importStar(require("redis"));
+const mysql = __importStar(require("mysql"));
 const axios_1 = __importDefault(require("axios"));
-const redisClient = redis_1.default.createClient({
+const redisClient = redis.createClient({
     url: process.env.REDIS_URL,
 });
-const mySQLClient = mysql_1.default.createConnection({
+const mySQLClient = mysql.createConnection({
     host: process.env.MY_SQL_HOST,
     port: Number(process.env.MY_SQL_PORT),
     user: process.env.MY_SQL_USERNAME,
